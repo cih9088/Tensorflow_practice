5e3d2dbecdd2340b343731a44d6ddec10bec2fba
Inhyuk Jo <ihcho@postech.ac.kr>
2017-03-30 14:12:50 +0900

    Initial commit

---
+++ ALI/main.py
@@ -0,0 +1,58 @@
+import os
+import sys
+from tf_utils.common import Logger
+
+from ALI import ALI
+
+import tensorflow as tf
+import pprint
+
+flags = tf.app.flags
+flags.DEFINE_integer('batch_size', 100, 'Size of batches to use')
+flags.DEFINE_float('g_lr', 0.0001, 'Learning rate for generator')
+flags.DEFINE_float('d_lr', 0.0001, 'Learning rate for discriminator')
+flags.DEFINE_float('beta1', 0.5, 'Momentum term of adamoptimizer')
+flags.DEFINE_integer('z_dim', 10, 'a number of z dimension layer')
+flags.DEFINE_string('z_dist', 'normal', 'Distribution for z [normal]')
+flags.DEFINE_string('log_dir', 'results_simple/', 'saved image directory')
+flags.DEFINE_integer('max_epoch', 500, 'A number of epochs to train')
+flags.DEFINE_boolean('is_train', False, 'True for training, False for testing')
+flags.DEFINE_string('data_dir', '/home/mlg/ihcho/data', 'data directory')
+flags.DEFINE_string('data', 'mnist', 'fuel data')
+flags.DEFINE_boolean('monitor', True, 'True for monitoring training process')
+FLAGS = flags.FLAGS
+
+
+def main(_):
+
+    if not os.path.exists(FLAGS.log_dir):
+        os.makedirs(FLAGS.log_dir)
+    f = open(os.path.join(FLAGS.log_dir, 'training.log'), 'a')
+    sys.stdout = Logger(f)
+
+    print('\n======================================')
+    pprint.PrettyPrinter().pprint(flags.FLAGS.__flags)
+
+    run_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    with tf.Session(config=run_config) as sess:
+        ali = ALI(
+            sess=sess,
+            log_dir=FLAGS.log_dir,
+            data=FLAGS.data,
+            data_dir=FLAGS.data_dir,
+            batch_size=FLAGS.batch_size,
+            z_dim=FLAGS.z_dim,
+            z_dist=FLAGS.z_dist
+        )
+
+        if FLAGS.is_train:
+            ali.train(FLAGS)
+        else:
+            if not ali.load_model(FLAGS.log_dir):
+                raise Exception('[!] Train a model first, then run test mode')
+
+    f.close()
+
+
+if __name__ == '__main__':
+    tf.app.run()
